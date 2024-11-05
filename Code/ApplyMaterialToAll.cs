ce4310a63f9d528dcd988b7673ab733e179879f0
Ramon Brullo <kingvanti650@gmail.com>
2024-11-05 15:53:46 +0100

    feat: add material appy logic

---
+++ Code/ApplyMaterialToAll.cs
@@ -0,0 +1,65 @@
+#nullable enable
+
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace At.Ac.FhStp.ApplyMaterialAll
+{
+    [InitializeOnLoad]
+    public static class ApplyMaterialToAll
+    {
+        private static GameObject? TryAsSelectedGameObject(int instanceID)
+        {
+            return EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+        }
+
+        private static Material? TryGetDraggedMaterial()
+        {
+            return DragAndDrop.objectReferences?.SingleOrDefault() as Material;
+        }
+
+        private static void OnHierarchyGUI(int instanceID, Rect selectionRect)
+        {
+            var currentEvent = Event.current;
+
+            // Must hold alt
+            if (!currentEvent.alt) return;
+
+            // Must only drag single material
+            if (TryGetDraggedMaterial() is not { } material) return;
+
+            // Must target game object
+            if (TryAsSelectedGameObject(instanceID) is not { } gameObject)
+                return;
+
+            // Handle drag updated for feedback
+            if (currentEvent.type == EventType.DragUpdated)
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                currentEvent.Use();
+                return;
+            }
+
+            // Must be drag
+            if (currentEvent.type != EventType.DragPerform) return;
+
+            DragAndDrop.AcceptDrag();
+
+            Undo.RegisterFullObjectHierarchyUndo(gameObject,
+                "Apply material to all");
+            var meshRenderers =
+                gameObject.GetComponentsInChildren<MeshRenderer>();
+            foreach (var meshRenderer in meshRenderers)
+                meshRenderer.material = material;
+            EditorUtility.SetDirty(gameObject);
+
+            currentEvent.Use();
+        }
+
+        static ApplyMaterialToAll()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI;
+        }
+    }
+}
